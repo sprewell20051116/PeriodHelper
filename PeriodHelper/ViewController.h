33f2a9847ba0fcdd22bb1a37243639a791cd91e5
GIGIGUN <sprewell20051116@gmail.com>
2017-04-22 11:01:35 +0800

    Initial Commit

---
+++ PeriodHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PeriodHelper
+//
+//  Created by GIGIGUN on 22/04/2017.
+//  Copyright © 2017 GIGIGUN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
